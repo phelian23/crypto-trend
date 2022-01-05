51049493b44028c6936f8fe9e75f9b4355af65c8
Oluwafemi <femikola23@gmail.com>
2022-01-05 14:10:24 +0400

    Update app

--- src/components/coins.js
+++ src/components/coins.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdArrowBackIos } from 'react-icons/md';
 import { FiSettings } from 'react-icons/fi';
 import { AiFillAudio } from 'react-icons/ai';
 import styles from '../styles/coin.module.css';
+import { getList, showCoinDet } from '../redux/details/details';
 
 const CoinsData = () => {
   const coinsdat = useSelector((state) => state.coinReducer);
   const selCoin = coinsdat.filter((league) => league.display === true)[0];
   const coinList = selCoin.coinArr;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getList(coinList));
+  }, []);
 
   /* eslint-disable no-nested-ternary */
   const approx = (value) => (value >= 1.0e+12
@@ -33,8 +40,24 @@ const CoinsData = () => {
     color: 'red',
   };
 
+  const filterCoin = () => {
+    let coinName = '';
+    const userInput = document.getElementById('coinSearch');
+    const coindata = userInput.value.toLowerCase();
+    const coinDetails = document.getElementsByClassName('coin-class');
+    [...coinDetails].forEach((coinnam) => {
+      const helpVar = coinnam;
+      coinName = helpVar.textContent || helpVar.inneText;
+      if (coinName.toLowerCase().indexOf(coindata) > -1) {
+        helpVar.style.display = '';
+      } else {
+        helpVar.style.display = 'none';
+      }
+    });
+  };
+
   return (
-    <div>
+    <div className={styles.backg}>
       <header className={styles.header}>
         <Link className={styles.link} to="/crypto-trend">
           <MdArrowBackIos />
@@ -46,35 +69,38 @@ const CoinsData = () => {
           <FiSettings />
         </div>
       </header>
+      <input type="text" className={styles.search} id="coinSearch" placeholder="Search Coin..." onKeyDown={filterCoin} />
       <h2 className={styles.main}>{selCoin.name}</h2>
       <ul className={styles.list}>
         {coinList.map((coin) => (
-          <li key={coin.id}>
-            <div className={styles.listItems}>
-              <h3>{coin.name}</h3>
-              <div>
-                <p>
-                  <span>Price: </span>
-                  <span>{othApprox(coin.priceUsd)}</span>
-                </p>
-                <p>
-                  <span>Market Cap: </span>
-                  <span>{approx(coin.marketCapUsd)}</span>
-                </p>
-                <p>
-                  <span>24Hr Change: </span>
-                  <span style={coin.changePercent24Hr > 0
-                    ? positive
-                    : coin.changePercent24Hr < 0
-                      ? negative
-                      : null}
-                  >
-                    {othApprox(coin.changePercent24Hr)}
-                  </span>
-                </p>
+          <Link key={coin.id} to={`/details/more/${coin.name}`} onClick={() => dispatch(showCoinDet(coin.id))}>
+            <li className="coin-class">
+              <div className={styles.listItems}>
+                <h3>{coin.name}</h3>
+                <div>
+                  <p>
+                    <span>Price: </span>
+                    <span>{othApprox(coin.priceUsd)}</span>
+                  </p>
+                  <p>
+                    <span>Market Cap: </span>
+                    <span>{approx(coin.marketCapUsd)}</span>
+                  </p>
+                  <p>
+                    <span>24Hr Change: </span>
+                    <span style={coin.changePercent24Hr > 0
+                      ? positive
+                      : coin.changePercent24Hr < 0
+                        ? negative
+                        : null}
+                    >
+                      {othApprox(coin.changePercent24Hr)}
+                    </span>
+                  </p>
+                </div>
               </div>
-            </div>
-          </li>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AllCoinsData from './allCoins';
 import CoinsData from './coins';
+import MoreDetails from './moreDetails';
 
 const HomePage = () => (
   <div>
     <Routes>
       <Route exact path="/crypto-trend" element={<AllCoinsData />} />
       <Route path="/details/:name" element={<CoinsData />} />
+      <Route path="/details/more/:name" element={<MoreDetails />} />
     </Routes>
   </div>
 );
